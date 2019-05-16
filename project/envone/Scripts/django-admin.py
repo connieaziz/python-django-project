43d5ece98d7a5837bfb357336a7cddce2e63febb
connieaziz <connieaziz@gmail.com>
2019-05-16 20:01:11 +0300

    added new file

---
+++ project/envone/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\student\desktop\python practise\django-projects\project\envone\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
