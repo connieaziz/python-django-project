3fa4f7b4a9fc365364160a8b58436fb1cff3bbf1
connieaziz <connieaziz@gmail.com>
2019-05-24 13:03:33 +0300

    stuent data

--- project2/Akirachix/Student/models.py
+++ project2/Akirachix/Student/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+	first_name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
+	date_of_birth = models.DateField()
+	registration_number = models.CharField(max_length=50)
+	place_of_residence = models.CharField(max_length=50)
+	phone_number = models.CharField(max_length=50)
+	email = models.CharField()
+	guardian_name = models.CharField(max_length=50)
+	id_number = models.CharField(max_length=50)
+	date_joined = models.integerField()
+		
